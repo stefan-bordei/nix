f168dbf9a64a190201ccefdd840e110a46017831
stefan-bordei <stefan.bordei13@gmail.com>
2023-12-15 18:17:32 +0000

    Updating terminal font to fix systemd-vconsole-setup.

--- machines/hivecluster/configuration.nix
+++ machines/hivecluster/configuration.nix
@@ -108,7 +108,7 @@ in
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
-    font = "consolas";
+    font = "Lat2-Terminus16";
     keyMap = "us";
   };
 
