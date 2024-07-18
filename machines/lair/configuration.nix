193692f3b6e7cc4148ca54e2fb3454bc4be20ed1
stefan-bordei <stefan.bordei13@gmail.com>
2024-07-18 18:48:49 +0100

    Add Docker to lair.

--- machines/lair/configuration.nix
+++ machines/lair/configuration.nix
@@ -125,6 +125,8 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  virtualisation.docker.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.zygot = {
     isNormalUser = true;
