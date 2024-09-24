cd064346ea73634f774c39a8c3499e7ac1f54f6c
stefan-bordei <stefan.bordei13@gmail.com>
2024-09-24 16:08:14 +0100

    Update flake and config.

--- machines/lair/configuration.nix
+++ machines/lair/configuration.nix
@@ -85,7 +85,7 @@
 
   hardware.opengl = {
     enable = true;
-    driSupport = true;
+    #driSupport = true;
     driSupport32Bit = true;
     extraPackages32 = with pkgs.pkgsi686Linux; [ libva driversi686Linux.amdvlk ]; # steam
     extraPackages = with pkgs; [ rocm-opencl-icd rocm-opencl-runtime amdvlk ];
@@ -106,7 +106,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
+  #sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
